e28c7890536a901b17ffb6b4d870d8f35a7d16ec
torniko <tornike.nafetvaridze@gmail.com>
2021-10-12 21:49:48 +0400

    connect leatherProductRepo to dbcontext

--- Alexanderi.ge/Models/LeatherProductRepository.cs
+++ Alexanderi.ge/Models/LeatherProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Alexanderi.ge.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,12 @@ namespace Alexanderi.ge.Models
 {
     public class LeatherProductRepository : ILeatherProductRepository
     {
+        private readonly AppDbContext context;
+
+        public LeatherProductRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
         public LeatherProduct CreateNew(LeatherProduct leather)
         {
             throw new NotImplementedException();
